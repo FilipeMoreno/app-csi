ca66ff3adfdf999bd4090d17f691519aef94eb3a
Filipe Moreno <y8.filipe@gmail.com>
2023-10-28 16:11:46 -0300

    bump version

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -38,9 +38,9 @@ export function Sidebar({ className }: SidebarProps) {
                 <g
                   id="Page-1"
                   stroke="none"
-                  stroke-width="1"
+                  strokeWidth="1"
                   fill="none"
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                 >
                   <g
                     id="Dribbble-Light-Preview"
