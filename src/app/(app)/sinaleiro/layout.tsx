3647d0881ff69612fde60fc88d3e4917efce59ee
Filipe Moreno <y8.filipe@gmail.com>
2023-11-09 11:37:41 -0300

    melhorias

--- src/app/(app)/sinaleiro/layout.tsx
+++ src/app/(app)/sinaleiro/layout.tsx
@@ -4,6 +4,9 @@ import '@/styles/globals.css'
 export const metadata: Metadata = {
   title: 'Sinaleiro - CSI',
   description: 'Aplicativo para controle de sinal',
+  openGraph: {
+    images: 'http://colegiosantoinacio.com.br/img/favicon.ico',
+  },
 }
 
 export default function SinaleiroLayout({
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  version: '1.0.22.1-alpha',
+  version: '1.0.25.1-alpha',
 }
