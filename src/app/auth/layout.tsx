b2d03bf22e1a66bff8a60111043d58cc15220c7e
Filipe Moreno <y8.filipe@gmail.com>
2023-11-10 01:53:34 -0300

    Update auth layout/page components

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,16 @@
 import { Metadata } from 'next'
+import NextTopLoader from 'nextjs-toploader'
 import { ReactNode } from 'react'
 
 export const metadata: Metadata = {
   title: 'Login - CSI',
+  description: 'Faça login na plataforma CSI',
 }
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-12 bg-background">
+      <NextTopLoader color="#af3c41" />
       {children}
     </div>
   )
