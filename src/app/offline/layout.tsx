c0c0d278a35c840307ff4871c0393a02973f5691
Filipe Moreno <y8.filipe@gmail.com>
2023-11-20 21:28:35 -0300

    Update sw.js

---
+++ src/app/offline/layout.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next'
+import NextTopLoader from 'nextjs-toploader'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Você está offline',
+}
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-12 bg-background">
+      <NextTopLoader color="#af3c41" />
+      {children}
+    </div>
+  )
+}
---
+++ src/app/offline/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Offline = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1>OFFLINE!</h1>
+    </div>
+  )
+}
+
+export default Offline
