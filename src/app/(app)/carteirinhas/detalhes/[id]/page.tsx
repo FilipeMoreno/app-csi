cfa20542ab324615552b9ba1f455df1cdd3e23dd
Filipe Moreno <y8.filipe@gmail.com>
2023-11-22 11:47:34 -0300

    Remove unused code and dependencies

--- src/app/(app)/carteirinhas/detalhes/[id]/page.tsx
+++ src/app/(app)/carteirinhas/detalhes/[id]/page.tsx
@@ -2,26 +2,9 @@
 
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
-import React, { useCallback, useRef, useState } from 'react'
-import Webcam from 'react-webcam'
 
 export default function CarteirinhasScanner() {
   const router = useRouter()
-
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: 'user',
-  }
-
-  const webcamRef = useRef<Webcam>(null)
-  const [url, setUrl] = useState<string | null>(null)
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot()
-    if (imageSrc) {
-      setUrl(imageSrc)
-    }
-  }, [webcamRef])
 
   return (
     <div>
