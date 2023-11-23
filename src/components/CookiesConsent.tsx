87045a75657aae3afa59c0f032361c4c803a9c3e
Filipe Moreno <y8.filipe@gmail.com>
2023-11-23 09:26:39 -0300

    fix cookie consent functionality

--- src/components/CookiesConsent.tsx
+++ src/components/CookiesConsent.tsx
@@ -19,12 +19,16 @@ export default function CookiesConsent() {
   }
   return (
     <div className="fixed inset-0 bg-zinc-950 bg-opacity-70">
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between bg-zinc-900 px-4 py-8">
+      <div
+        onClick={() => acceptCookie()}
+        className="fixed bottom-0 left-0 right-0 flex items-center justify-between bg-zinc-900 px-4 py-8"
+      >
         <span className="text-dark mx-4 mr-16 text-base">
           Este site utiliza cookies para melhorar a experiência do usuário. Ao
           utilizar o nosso site você concorda com todos os cookies de acordo com
           a nossa Política de Cookies.
         </span>
+
         <Button className="w-64" onClick={() => acceptCookie()}>
           Aceito
         </Button>
