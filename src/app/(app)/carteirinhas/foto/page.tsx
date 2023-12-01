9da474f1d6bf9fd774d6f3a2117acf8faf733b46
Filipe Moreno <y8.filipe@gmail.com>
2023-12-01 00:44:19 -0300

    teste

--- src/app/(app)/carteirinhas/foto/page.tsx
+++ src/app/(app)/carteirinhas/foto/page.tsx
@@ -23,24 +23,26 @@ export default function CarteirinhasFoto() {
 
   return (
     <div className="h-full w-full rounded-lg bg-secondary p-4">
-      <Camera
-        ref={camera}
-        aspectRatio="cover"
-        // numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
-        // videoSourceDeviceId={activeDeviceId}
-        errorMessages={{
-          noCameraAccessible:
-            'No camera device accessible. Please connect your camera or try a different browser.',
-          permissionDenied:
-            'Permission denied. Please refresh and give camera permission.',
-          switchCamera:
-            'It is not possible to switch camera to different one because there is only one video device accessible.',
-          canvas: 'Canvas is not supported.',
-        }}
-        videoReadyCallback={() => {
-          console.log('Video feed ready.')
-        }}
-      />
+      <div className="h-16 w-16">
+        <Camera
+          ref={camera}
+          aspectRatio="cover"
+          // numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
+          // videoSourceDeviceId={activeDeviceId}
+          errorMessages={{
+            noCameraAccessible:
+              'No camera device accessible. Please connect your camera or try a different browser.',
+            permissionDenied:
+              'Permission denied. Please refresh and give camera permission.',
+            switchCamera:
+              'It is not possible to switch camera to different one because there is only one video device accessible.',
+            canvas: 'Canvas is not supported.',
+          }}
+          videoReadyCallback={() => {
+            console.log('Video feed ready.')
+          }}
+        />
+      </div>
       {camera?.current?.flashStatus() ? (
         <button
           onClick={() => {
