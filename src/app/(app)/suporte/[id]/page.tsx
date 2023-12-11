f464487851c8bc59a2c0539f48154f4790136e76
Filipe Moreno <y8.filipe@gmail.com>
2023-12-11 14:12:59 -0300

    atualizações

--- src/app/(app)/suporte/[id]/page.tsx
+++ src/app/(app)/suporte/[id]/page.tsx
@@ -11,8 +11,18 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
-import { SendHorizonal, SmileIcon } from 'lucide-react'
+import { PaperclipIcon, SendHorizonal, SmileIcon } from 'lucide-react'
 import Image from 'next/image'
 
 export default function ChamadosDetalhes() {
@@ -21,32 +31,11 @@ export default function ChamadosDetalhes() {
       <div className="flex flex-col space-y-2">
         <Card className="w-full lg:w-[300px]">
           <CardHeader>
-            <div className="flex flex-col space-y-2">
-              <span className="font-bold">
-                Projetor não funciona <Badge className="ml-2">#1234</Badge>
-              </span>
-              <div className="flex flex-row space-x-2">
-                <Badge
-                  variant={'outline'}
-                  className="border-green-500 font-bold text-green-500"
-                >
-                  Aberto
-                </Badge>
-                <Badge
-                  variant={'outline'}
-                  className="border-red-500 font-bold text-red-500"
-                >
-                  Crítica
-                </Badge>
-              </div>
-            </div>
+            <CardTitle className="font-bold">
+              Informações do chamado #123456
+            </CardTitle>
           </CardHeader>
-        </Card>
-        <Card className="w-full lg:w-[300px]">
-          <CardHeader>
-            <CardTitle className="font-bold">Informações</CardTitle>
-          </CardHeader>
-          <CardContent>
+          <CardContent className="space-y-2">
             <div className="flex flex-col space-y-1">
               <span className="text-sm">Data de abertura</span>
               <span className="text-xs text-zinc-500">01/01/2024 às 00:00</span>
@@ -57,7 +46,49 @@ export default function ChamadosDetalhes() {
             </div>
             <div className="flex flex-col space-y-1">
               <span className="text-sm">Setor</span>
-              <span className="text-xs text-zinc-500">Usuário</span>
+              <Select defaultValue="sala-de-aula">
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione o setor" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="sala-de-aula">Sala de aula</SelectItem>
+                    <SelectItem value="salao">Salão</SelectItem>
+                    <SelectItem value="patio">Pátio</SelectItem>
+                    <SelectItem value="quadra">Quadra</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <span className="text-sm">Prioridade</span>
+              <Select defaultValue="critica">
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione a prioridade" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="baixa">Baixa</SelectItem>
+                    <SelectItem value="moderada">Moderada</SelectItem>
+                    <SelectItem value="alta">Alta</SelectItem>
+                    <SelectItem value="critica">Crítica</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <span className="text-sm">Estado</span>
+              <Select defaultValue="aberto">
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione o estado" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="aberto">Aberto</SelectItem>
+                    <SelectItem value="fechado">Fechado</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </CardContent>
         </Card>
@@ -98,6 +129,33 @@ export default function ChamadosDetalhes() {
         </Card>
       </div>
       <section className="flex w-full flex-col rounded-lg border">
+        <div className="flex flex-row items-center justify-between border-b">
+          <div className="flex flex-col p-4">
+            <span className="text-lg font-bold">
+              Projetor não funciona
+              <Badge variant={'outline'} className="ml-2 text-xs">
+                #1234
+              </Badge>
+            </span>
+            <div className="mt-2 flex flex-row space-x-2">
+              <Badge
+                variant={'outline'}
+                className="border-green-500 font-bold text-green-500"
+              >
+                Aberto
+              </Badge>
+              <Badge
+                variant={'outline'}
+                className="border-red-500 font-bold text-red-500"
+              >
+                Crítica
+              </Badge>
+            </div>
+          </div>
+          <div className="mx-4">
+            <Button variant={'outline'}>Fechar chamado</Button>
+          </div>
+        </div>
         <main className="flex-1 overflow-auto p-4">
           <div className="space-y-4">
             <div className="flex items-end gap-2">
@@ -111,23 +169,55 @@ export default function ChamadosDetalhes() {
               </div>
               <div className="flex flex-col space-y-1">
                 <div className="rounded-lg bg-secondary p-2">
+                  <span className="flex text-[10px] font-bold text-zinc-400">
+                    Usuário
+                  </span>
                   <p className="text-sm">
                     Preciso de ajuda! O computador na sala 1 parou de funcionar.
                   </p>
+                  <span className="flex justify-end text-[10px] text-zinc-400">
+                    01/01/2024 às 00:00
+                  </span>
                 </div>
-                <span className="text-[10px] text-zinc-500">
-                  Usuário - 01/01/2024 às 00:00
-                </span>
               </div>
             </div>
             <div className="flex items-end justify-end gap-2">
               <div className="flex flex-col space-y-1">
                 <div className="rounded-lg bg-tertiary p-2 text-white">
+                  <span className="flex text-[10px] font-bold text-zinc-300">
+                    Usuário
+                  </span>
                   <p className="text-sm">Resolvido!</p>
+                  <span className="flex justify-end text-[10px] text-zinc-300">
+                    01/01/2024 às 00:00
+                  </span>
                 </div>
-                <span className="text-[10px] text-zinc-500">
-                  Usuário - 01/01/2024 às 00:00
-                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <Avatar>
+                  <AvatarFallback>
+                    <SmileIcon />
+                  </AvatarFallback>
+                  <AvatarImage src="/icons/icon.png" />
+                </Avatar>
+              </div>
+            </div>
+            <div className="flex items-end justify-end gap-2">
+              <div className="flex flex-col space-y-1">
+                <div className="rounded-lg bg-tertiary p-2 text-white">
+                  <span className="flex text-[10px] font-bold text-zinc-300">
+                    Usuário
+                  </span>
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sunt reprehenderit a culpa eligendi voluptas at. Temporibus
+                    ipsum architecto repellat non. Quasi, aperiam iste. Sapiente
+                    qui optio repudiandae amet adipisci nesciunt!
+                  </p>
+                  <span className="flex justify-end text-[10px] text-zinc-300">
+                    01/01/2024 às 00:00
+                  </span>
+                </div>
               </div>
               <div className="flex items-center justify-center">
                 <Avatar>
@@ -141,11 +231,13 @@ export default function ChamadosDetalhes() {
           </div>
         </main>
         <footer className="p-4">
-          <div className="flex items-center gap-2">
+          <div className="flex items-end gap-2">
             <Textarea className="flex-1" placeholder="Digite sua mensagem..." />
-            <Button>
-              <SendHorizonal className="mr-2 h-4 w-4" />
-              Enviar
+            <Button variant={'ghost'} size={'icon'}>
+              <PaperclipIcon className="h-4 w-4" />
+            </Button>
+            <Button className="rounded-full" size={'icon'}>
+              <SendHorizonal className="h-4 w-4" />
             </Button>
           </div>
         </footer>
