73f7e230fae01a7818fbe1c8e7f972dbe00d232f
Filipe Moreno <y8.filipe@gmail.com>
2023-12-11 22:39:45 -0300

    add: Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Metadata } from 'next'
 import Script from 'next/script'
@@ -100,6 +101,7 @@ export default function RootLayout({
 
         {children}
         <SpeedInsights />
+        <Analytics />
       </head>
     </html>
   )
