a8a0f44e460e66fa6c1a14c0816a99c770251b70
Filipe Moreno <y8.filipe@gmail.com>
2024-02-07 20:45:53 -0300

    Atualizações na página de ações do scanner de carteirinhas

--- src/app/(app)/carteirinhas/scanner/acoes/page.tsx
+++ src/app/(app)/carteirinhas/scanner/acoes/page.tsx
@@ -123,7 +123,7 @@ export default function CarteirinhasScannerAcoes() {
 									<DrawerContent className="flex flex-col rounded-t-[10px] h-full mt-24 max-h-[46%] fixed bottom-0 left-0 right-0 p-4">
 										<div className="rounded-t-[10px] flex-1">
 											<div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full mb-8" />
-											<div className="max-w-md mx-auto">
+											<div className="max-w-md mx-auto p-4">
 												<DrawerTitle className="font-medium mb-4">
 													Informações da carteirinha
 												</DrawerTitle>
@@ -133,7 +133,8 @@ export default function CarteirinhasScannerAcoes() {
 												<p className="text-zinc-300 mb-2">
 													Série/Turma: <b>1º ANO A - Ensino Médio</b>
 												</p>
-												<p className="text-gray-600 mb-2">
+												<Separator />
+												<p className="text-zinc-300 mb-2">
 													Selecione o status que deseja adicionar
 												</p>
 											</div>
